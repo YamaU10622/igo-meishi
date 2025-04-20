b3b7e741b2612494a56d4fbe88aa69c01c523451
YamaU10622 <tokuyama.yudai.jp@alumni.tsukuba.ac.jp>
2025-04-21 03:08:51 +0900

    コメント・フッター編集

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="bottom-0 w-full mt-12 py-6 text-center text-sm text-black-500">
+    <footer className="bottom-0 w-full mt-12 py-6 text-center text-xs text-black-500">
       <p>© 2025 囲碁名刺サイト</p>
       <p>制作者：ごいごい</p>
-      <p className="mt-1">
-        <a href="https://x.com/GoiGoi_5151" className="hover:underline">X(Twitter)</a>
-      </p>
+      <p>お問い合わせやご意見などは<a href="https://x.com/GoiGoi_5151" className="hover:underline">X(Twitter)</a>のDMで連絡して下さい</p>
     </footer>
   );
 }
