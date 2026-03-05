f9238d948424a63979ed56153d8e3a87c9deca94
YamaU <97282284+YamaU10622@users.noreply.github.com>
2026-03-05 22:33:25 +0900

    Revise Footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,6 @@ export default function Footer() {
   return (
     <footer className="bottom-0 w-full mt-12 py-6 text-center text-xs text-black-500">
       <p>© 2025 囲碁名刺サイト</p>
-      <p>制作者：ごいごい</p>
-      <p>お問い合わせやご意見などは<a href="https://x.com/GoiGoi_5151" className="underline">X(Twitter)</a>のDMで連絡してください</p>
     </footer>
   );
 }
